771b4bbb59ef462dc46a5d8f04f88f3bfb2b4930
chinhbean09 <chinh0726@gmail.com>
2025-02-20 14:49:12 +0700

    first commit

---
+++ src/main/java/com/chinhbean/realtimechat/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.chinhbean.realtimechat.config;
+
+import com.chinhbean.realtimechat.interceptor.HttpHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+
+//Annotation này nói với Spring rằng hãy bật (enable) WebSocket Server.
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private HttpHandshakeInterceptor handshakeInterceptor;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS().setInterceptors(handshakeInterceptor);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+
+}
